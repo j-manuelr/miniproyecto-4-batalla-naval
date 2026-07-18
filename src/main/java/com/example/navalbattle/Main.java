ee4a22ced4e7753d1e7d22004c3f95e733d92f21
carlosk4 <lorenzohinestroza44@gmail.com>
2026-07-18 18:58:27 -0500

    vista incial

--- src/main/java/com/example/navalbattle/Main.java
+++ src/main/java/com/example/navalbattle/Main.java
@@ -1,17 +1,26 @@
 package com.example.navalbattle;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import static javafx.application.Application.launch;
 
 public class Main extends Application {
 
     @Override
-    public  void start (Stage stage){
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("battle-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1120, 740);
         stage.setTitle("Batalla Naval");
+        stage.setResizable(false);
+        stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         launch(args);
     }
