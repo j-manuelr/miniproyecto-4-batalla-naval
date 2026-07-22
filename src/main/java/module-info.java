9ce27e99d7a8af797ee626d43a67b22cf157ac70
Conb3c <juseduqu@gmail.com>
2026-07-22 17:39:48 -0500

    Fixes in views and Controllers done.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,9 @@ module com.example.navalbattle {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.navalbattle to javafx.fxml;
+    opens com.example.navalbattle.controller to javafx.fxml;
+
     exports com.example.navalbattle;
+    exports com.example.navalbattle.controller;
 }
